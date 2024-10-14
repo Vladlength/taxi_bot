4d82fd13fd3b5013d9861a3262aeb58e2630230f
Vlad <129696560+Vladlength@users.noreply.github.com>
2024-10-14 19:07:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TELEGRAM_API = "7214739665:AAFpyVXzV1GU90DyFbXLIt7FIEnC4tT_xAQ"
-YANDEX_CATALOG = "b1gkrgct6e0fa5u0udsu"
-KEY_ID = "ajeec4attotsjcmb5deg"
-SECRET_KEY = "AQVNzmEV3uO8myyytdDC1DqMtnx8fLxvUPQNUvoT"
+TELEGRAM_API = "secret"
+YANDEX_CATALOG = "secret"
+KEY_ID = "secret"
+SECRET_KEY = "secret"
